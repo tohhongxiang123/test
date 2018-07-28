a86e79956f8721146bec89fd9d1f23e9e2762af8
tohhongxiang123 <tohhongxiang@gmail.com>
2018-07-28 23:27:54 +0800

    Update singly linked list.c

--- singly linked list.c
+++ singly linked list.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-#include <cs50.h>
+#include <stdlib.h>
+typedef int bool;
+#define true = 1;
+#define false = 0;
+
 
 typedef struct sllist { //contains 2 things: value and a next pointer
     int val;
